f4ac18867358bc968d726977d67ce98776f537f4
my0066667 <tranmy290404@gmail.com>
2025-06-02 00:38:48 +0700

    Giao diện
    Giao diện màn hình chính

---
+++ HomePage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using PhanMemQuanLyTaiSan.viewmodel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PhanMemQuanLyTaiSan.view
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Window
+    {
+        public HomePage()
+        {
+
+            InitializeComponent();
+            this.DataContext = new MainViewModel();
+        }
+    }
+}
